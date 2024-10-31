21d9ff16eb70029c056a007554d0c22c6e4ea80c
William Taylor <williammarktaylor@gmail.com>
2024-11-01 00:07:59 +1300

    Finished animation

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,11 @@
 
 hd44780_I2Cexp lcd(0x27, 16, 2);
 
-int topRowAnimationSequence[] = { 5, 5, 3, 4, 1, 2, 5, 5, 5, 5, 3, 4, 1, 2, 5, 5, 5, 5, 3, 4, 1, 2, 5, 5, 5, 5, 3, 4, 1, 2, 5, 5, };
-int bottomRowAnimationSequence[] = { 3, 4, 0, 0, 0, 0, 1, 2, 3, 4, 0, 0, 0, 0, 1, 2, 3, 4, 0, 0, 0, 0, 1, 2, 3, 4, 0, 0, 0, 0, 1, 2, };
+#define ANIM_FRAME_COUNT      8
+#define LCD_COLUMNS           16
+#define LCD_ROWS              2
 
+// The custom chars that make up the various animation frames.
 const byte animationCustomChars[][8] =
 {
   {
@@ -74,9 +76,21 @@ const byte animationCustomChars[][8] =
   }
 };
 
+// The characters that a single column must progress through in completion for one cycle of the animation. First digit is charater for upper row, second digit is for lower row.
+const int animationSequence[ANIM_FRAME_COUNT][LCD_ROWS] =
+{
+  { 5, 3 },
+  { 5, 4 },
+  { 3, 0 },
+  { 4, 0 },
+  { 1, 0 },
+  { 2, 0 },
+  { 5, 1 },
+  { 5, 2 },
+};
+
 void setup() {
   Serial.begin(9600);
-   // lcd.begin();
   lcd.init();
   lcd.backlight();
 
@@ -85,30 +99,30 @@ void setup() {
     lcd.createChar(i, animationCustomChars[i]);
   }
 
+  for (int i = 0; i < 3; i++) {
+    for (int frame = 0; frame < ANIM_FRAME_COUNT; frame++) {  // For each frame in the animation...
+      for (int column = 0; column < LCD_COLUMNS; column++) {  // For each column in the display...
+        int frameIndex = column;                              // Start with the column index, this means for a frame array containing ['\', '_', '/'] the first three columns will show \_/.
+        while (frameIndex >= ANIM_FRAME_COUNT) {              // There may be more columns than animation frames, if so pull it back to within the bounds of the animation frame array.
+          frameIndex -= ANIM_FRAME_COUNT;                     // For a 16 column display, the frame index will now be 0,1,2,3,4,5,6,7,0,1,2,3,4,5,6,7 for each of the 16 columns respectively.
+        }
+        frameIndex -= frame;                                  // Shift the frame index left by the number of frames already passed in the animation.
+                                                              // For frame 0: Col 3 has frameIndex 3, Col 4 has frameIndex 4 etc. For frame 1: Col 3 has frameIndex 2, Col 4 has frameIndex 3 etc.
+                                                              // This means for each frame in the animation, the char displayed on a column is the one previously displayed on the column to its left, creating the illusion of rightwards movement.
+        if (frameIndex < 0) {                                 // If left shifted earlier than the first frame, wrap back around to the last frame. In previous example in frame 2, Col 1 has frameIndex -1 which is wrapped around to frameIndex 7.
+          frameIndex += 8;
+        }
+        for (int row = 0; row < LCD_ROWS; row ++) {           // Draw the correct character in the upper and lower rows for that column.
+          lcd.setCursor(column, row);
+          lcd.write(animationSequence[frameIndex][row]);
+        }
+    }
+    delay(100);
+    }
+  }
   lcd.clear();
- }
+}
 
 void loop() { 
-  for (int z = 0; z < 8; z++) { // For each frame in the animation (8 frames, enough time for each square to show each of the 8 animation frames)
-    // i = z;
-    for (int k = 0; k < 16; k++) { // For each square...
-      int frameIndex = k;
-      while (frameIndex >= 8) {
-        frameIndex -= 8;
-      }
-      // frameIndex is now 0,1,2,3,4,5,6,7,0,1,2,3,4,5,6,7
-      frameIndex -= z;
-      if (frameIndex < 0) {
-        frameIndex += 8;
-      }
-      lcd.setCursor(k, 0);
-      lcd.write(topRowAnimationSequence[frameIndex]);
-      lcd.setCursor(k, 1);
-      lcd.write(bottomRowAnimationSequence[frameIndex]);
-      // i++;
-      // delay(1000);
-   }
-   delay(100);
-   lcd.clear(); 
-  }
+
 }
