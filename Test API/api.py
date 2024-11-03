4a2da2a4241010a62ce7cf951c820a60a134d402
William Taylor <williammarktaylor@gmail.com>
2024-11-03 22:30:14 +1300

    Updating with some dev certs

---
+++ Test API/api.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+import ssl
+
+# creating API
+app = FastAPI()
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+ssl_context.load_cert_chain('./cert.pem', keyfile='./key.pem')
+
+@app.get("/api/test", response_class=PlainTextResponse)
+async def getNumber():
+    return "*123|456|789"
---
+++ Test API/start.bat
@@ -0,0 +1,2 @@
+uvicorn api:app --ssl-keyfile ./key.pem --ssl-certfile ./cert.pem --port 8000 --host 10.11.12.1  --reload
+cmd /k
