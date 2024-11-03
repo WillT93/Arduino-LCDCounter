410ecb3d4524ccc3d2ce1d446a19c98f384dab14
William Taylor <williammarktaylor@gmail.com>
2024-11-03 16:12:12 +1300

    REmoved old LDR code

--- include/ldr_t93.h
+++ include/ldr_t93.h
@@ -4,12 +4,7 @@
 void InitializeLDR();
 void ProcessLDR();
 void UpdateBacklightPerLightLevel();
-void CalibrateLDRSwipeThreshold();
 bool LDRBelowDarkRoomThreshold();
 bool LDRAboveLightRoomThreshold();
-bool LDRBelowSwipeThreshold();
-void ProcessSwiping();
-void LDRSwiped();
-void LDRPostSwipeTimoutElapsed();
 
 #endif
